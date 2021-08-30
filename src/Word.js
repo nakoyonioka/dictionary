46320e433ff098fdd224f8ed04ee9450b8d102fd
nakopine <pineapplecaan@gmail.com>
2021-08-30 19:38:30 +0200

    improved styles

--- src/Word.js
+++ src/Word.js
@@ -1,38 +1,41 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
 
+export default function Word({ data }) {
+  return (
+    <div className="containerWord">
+      {data == null ? null : (
+        <>
+          <div className="word">{data.word}</div>
 
-export default function Word({data}) {
-   return (
-      <div className="containerWord">
-         {data==null?null:(<>
-         <div className="word">
-            {data.word}
-         </div>
-         <div className="origin">
-            <p>ORIGIN: {data.origin}</p>
-         </div>
-         <div className="audioSound">
+          <div className="origin">
+            <p>
+              ORIGIN: <span>{data.origin}</span>
+            </p>
+          </div>
+
+          <div className="audioSound">
             <audio controls autoPlay>
-               <source src={data.phonetics[0].audio} type="audio/mpeg" />
+              <source src={data.phonetics[0].audio} type="audio/mpeg" />
             </audio>
-         </div>
-         <div className="meanings">
-            {data.meanings.map(element => (
-               <>
-                  <div className="partOfSpeech">
-                     <p>PART OF SPEECH: {element.partOfSpeech}</p>
-                  </div>
-                  <div className="definition">
-                     <p>DEFINITION: {element.definitions[0].definition}</p>    
-                  </div>
-               </>
-            ))}
-         </div>
-         
-         </>
-         )}
-      </div>
+          </div>
 
-   )
+          <div className="meanings">
+            {data.meanings.map((element) => (
+              <>
+                <div className="bar"></div>
+                <div className="partOfSpeech">
+                  <span>{element.partOfSpeech}:</span>
+                </div>
+                <div className="definition">
+                  <p>{element.definitions[0].definition}</p>
+                </div>
+                <div className="bar"></div>
+              </>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
